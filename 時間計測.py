5a0cb37a7548527df02e92d0ac3e2f6a9530b7e8
Takahiro Namatame <103984069+Taka0007@users.noreply.github.com>
2022-10-02 10:39:52 +0900

    Time Measurement
    Program to measure execution time.

---
+++ 時間計測.py
@@ -0,0 +1,13 @@
+
+import time
+
+if __name__ == '__main__':
+    start = time.time()
+    
+
+    ###ここに操作を記入
+    
+    
+    
+    elapsed_time = time.time() - start
+    print ("経過時間:{0}".format(elapsed_time) + "[sec]")
