1d65fc2ec99980fdf37089eeb2ecce11cc801ae3
Taka <103984069+Taka0007@users.noreply.github.com>
2022-10-19 13:54:08 +0900

    hexadecimal conversion

---
+++ hexadecimal conversion.py
@@ -0,0 +1,17 @@
+def base_n(num_10,n):
+    str_n = ''
+    while num_10:
+        if num_10%n>=10:
+            return -1
+        str_n += str(num_10%n)
+        num_10 //= n
+    return int(str_n[::-1])
+    
+    
+    
+def base_10(num_n,n):
+    num_10 = 0
+    for s in str(num_n):
+        num_10 *= n
+        num_10 += int(s)
+    return num_10
